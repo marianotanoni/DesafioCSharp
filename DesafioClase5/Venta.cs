5c2a5c4357b60145b72e5df7be3ae3cd60ba3337
marianotanoni <70734478+marianotanoni@users.noreply.github.com>
2024-05-14 22:26:48 -0300

    Add files via upload

---
+++ DesafioClase5/Venta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioClase5
+{
+    public class Venta
+    {
+        private int _Id; 
+        private string _Comentarios;
+        private string _IdUsuario;
+
+        public Venta(int Id, string Comentarios,string IdUsuario)
+        {
+            this._Id = Id;
+            this._Comentarios = Comentarios;
+            this._IdUsuario= IdUsuario;
+        }
+        public int Id
+        { 
+            get { return _Id; }
+            set { _Id = value; }    
+        }
+        public string Comentarios
+        {
+            get { return _Comentarios; }
+            set {  this._Comentarios = value; }
+        }
+        public string IdUsuarios 
+        { 
+            get { return _IdUsuario; }
+            set { this._IdUsuario = value; }
+        }
+        
+    }
+}
